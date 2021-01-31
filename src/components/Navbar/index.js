4e7fa333615a184c57f7e7d8a6abbaed6d520791
toriomara <budkevich.vladimir@gmail.com>
2021-01-31 21:36:43 +0300

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav, NavLink} from "./NavbarElements"
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>Korund</NavLink>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
